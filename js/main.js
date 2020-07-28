faf007f19e8ddade89611a938714064d8935cd08
soyox <soyox2037@gmail.com>
2020-07-27 21:00:19 -0400

    +mods

--- js/main.js
+++ js/main.js
@@ -300,8 +300,8 @@ $('#screenshot-carusel').owlCarousel({
   cursor:false,
   lifeLike:true
 })
-  .type("<h1>¡Hola!,", {delay: 100})
-  .type(" Soy Mario Echeverria CEO de",{delay:100})
+  .type("<h1>¡Hola!,</h1>", {delay: 100})
+  .type(" <h1>Soy Mario Echeverria CEO de</h1>",{delay:100})
   .type("<br> <span>ME Agency.</span></h1>",{delay:100})
   .type("Somos la solucion para hacer realidad tu sueño, solo si quieres tener precencia en internet tener tu propia tienda, o una revista digital trabajemos juntos")
   .go();
