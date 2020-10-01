8a41b6108f6a783824159a846a9675f066d5caec
soyox <soyox2037@gmail.com>
2020-09-30 21:16:59 -0400

    update

--- js/main.js
+++ js/main.js
@@ -308,7 +308,7 @@ $('#screenshot-carusel').owlCarousel({
 
 
 //-------------map------------//
-var mymap = L.map('map').setView([51.505, -0.09], 13);
+var mymap = L.map('map').setView([10.6417, -71.6295], 13);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
