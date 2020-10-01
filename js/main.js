35e89095d87b59c04d6123b7954f6df03cd276be
soyox <soyox2037@gmail.com>
2020-09-30 21:42:46 -0400

    update

--- js/main.js
+++ js/main.js
@@ -316,12 +316,13 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1Ijoic295b3giLCJhIjoiY2tmMXR1c2hpMjJ1ZTJ6bDltbGVobXF1bCJ9.J10wJSUcOZHap6PF7Aryxg'
-})
+});
 L.marker([10.6417, -71.6295], {
   icon: L.mapbox.marker.icon({
       'marker-size': 'large',
       'marker-symbol': 'bus',
       'marker-color': '#fa0'
-  }).addTo(mymap);
+  })
+}).addTo(mymap);
 
 })(jQuery);
