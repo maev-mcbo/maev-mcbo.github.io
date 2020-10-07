9821c277e586b787c8ff1ce818534c7cbb5315bc
soyox <soyox2037@gmail.com>
2020-10-07 10:34:10 -0400

    test

--- js/main.js
+++ js/main.js
@@ -260,36 +260,7 @@ $('#screenshot-carusel').owlCarousel({
   $(window).stellar();
 
 
-  // ------------------------------------------------------------------------------ //
-  // Contact Form
-  // ------------------------------------------------------------------------------ //
-
-  var submitContact = $('#submit-message'),
-    message = $('#msg');
-
-  submitContact.on('click', function (e) {
-    e.preventDefault();
-
-    var $this = $(this);
-
-    $.ajax({
-      type: "POST",
-      url: 'contact.php',
-      dataType: 'json',
-      cache: false,
-      data: $('#contact-form').serialize(),
-      success: (data) => {
-
-        if (data.info !== 'error') {
-          $this.parents('form').find('input[type=text],input[type=email],textarea,select').filter(':visible').val('');
-          message.hide().removeClass('success').removeClass('error').addClass('success').html(data.msg).fadeIn('slow').delay(5000).fadeOut('slow');
-        } else {
-          message.hide().removeClass('success').removeClass('error').addClass('error').html(data.msg).fadeIn('slow').delay(5000).fadeOut('slow');
-        }
-      }
-    });
-  });
-
+  
   // ------------------------------------------------------------------------------ //
   // typeeeeit
   // --
